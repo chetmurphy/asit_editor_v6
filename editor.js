ebb6645c3cb7563b056ed537a2c92c2199731b44
Chet Murphy <chetmurphy@hotmail.com>
2023-03-05 09:03:38 -0800

    Updates

--- editor.js
+++ editor.js
@@ -157,7 +157,7 @@ editor.setOptions({
     enableBasicAutocompletion: true,
     enableSnippets: true,
     enableLiveAutocompletion: false,
-    autoScrollEditorIntoView: true,
+    //autoScrollEditorIntoView: true,
     vScrollBarAlwaysVisible: true
     
 })
