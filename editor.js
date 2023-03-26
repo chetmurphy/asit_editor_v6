012cecc09f75b076f8e0f11a3b0c7f5c330e78d7
Chet Murphy <chetmurphy@hotmail.com>
2023-03-26 08:34:02 -0700

    Updates

--- editor.js
+++ editor.js
@@ -175,7 +175,7 @@ editor.setOptions({
 editor.setTheme("ace/theme/chrome");
 editor.session.setMode("ace/mode/yaml");
 editor.session.on('change', autosave); //TBD restore and implement
-console.log('Editor created 54');
+console.log('Editor created 55');
 
 
 function adjust_tab_size(ac_data, indent) {
